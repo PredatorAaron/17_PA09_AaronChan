f5567c4ec7f00c27b8100af40a20b1d679a88969
iPR3DATOR <predatoraaron38@gmail.com>
2020-06-16 10:54:33 +0800

    First version of the project, created by AaronChan

---
+++ 17_PA9_AaronChan/17_PA9_AaronChan/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace _17_PA9_AaronChan
+{
+    partial class txt_convetedAmt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_amount = new System.Windows.Forms.Label();
+            this.lbl_selectcurrency = new System.Windows.Forms.Label();
+            this.lbl_value = new System.Windows.Forms.Label();
+            this.txt_amount = new System.Windows.Forms.TextBox();
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.rdb_USdollars = new System.Windows.Forms.RadioButton();
+            this.rdb_JapaneseYen = new System.Windows.Forms.RadioButton();
+            this.txt_convertedAmt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_amount
+            // 
+            this.lbl_amount.AutoSize = true;
+            this.lbl_amount.Location = new System.Drawing.Point(228, 85);
+            this.lbl_amount.Name = "lbl_amount";
+            this.lbl_amount.Size = new System.Drawing.Size(43, 13);
+            this.lbl_amount.TabIndex = 0;
+            this.lbl_amount.Text = "Amount";
+            // 
+            // lbl_selectcurrency
+            // 
+            this.lbl_selectcurrency.AutoSize = true;
+            this.lbl_selectcurrency.Location = new System.Drawing.Point(228, 143);
+            this.lbl_selectcurrency.Name = "lbl_selectcurrency";
+            this.lbl_selectcurrency.Size = new System.Drawing.Size(82, 13);
+            this.lbl_selectcurrency.TabIndex = 0;
+            this.lbl_selectcurrency.Text = "Select Currency";
+            // 
+            // lbl_value
+            // 
+            this.lbl_value.AutoSize = true;
+            this.lbl_value.Location = new System.Drawing.Point(228, 235);
+            this.lbl_value.Name = "lbl_value";
+            this.lbl_value.Size = new System.Drawing.Size(34, 13);
+            this.lbl_value.TabIndex = 0;
+            this.lbl_value.Text = "Value";
+            // 
+            // txt_amount
+            // 
+            this.txt_amount.Location = new System.Drawing.Point(323, 82);
+            this.txt_amount.Name = "txt_amount";
+            this.txt_amount.Size = new System.Drawing.Size(198, 20);
+            this.txt_amount.TabIndex = 1;
+            this.txt_amount.TextChanged += new System.EventHandler(this.txt_amount_TextChanged);
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(525, 138);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(75, 23);
+            this.btn_convert.TabIndex = 3;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.btn_convert_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(525, 171);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // rdb_USdollars
+            // 
+            this.rdb_USdollars.AutoSize = true;
+            this.rdb_USdollars.Location = new System.Drawing.Point(355, 174);
+            this.rdb_USdollars.Name = "rdb_USdollars";
+            this.rdb_USdollars.Size = new System.Drawing.Size(75, 17);
+            this.rdb_USdollars.TabIndex = 5;
+            this.rdb_USdollars.TabStop = true;
+            this.rdb_USdollars.Text = "US Dollars";
+            this.rdb_USdollars.UseVisualStyleBackColor = true;
+            // 
+            // rdb_JapaneseYen
+            // 
+            this.rdb_JapaneseYen.AutoSize = true;
+            this.rdb_JapaneseYen.Location = new System.Drawing.Point(355, 141);
+            this.rdb_JapaneseYen.Name = "rdb_JapaneseYen";
+            this.rdb_JapaneseYen.Size = new System.Drawing.Size(93, 17);
+            this.rdb_JapaneseYen.TabIndex = 5;
+            this.rdb_JapaneseYen.TabStop = true;
+            this.rdb_JapaneseYen.Text = "Japanese Yen";
+            this.rdb_JapaneseYen.UseVisualStyleBackColor = true;
+            // 
+            // txt_convertedAmt
+            // 
+            this.txt_convertedAmt.Location = new System.Drawing.Point(323, 232);
+            this.txt_convertedAmt.Name = "txt_convertedAmt";
+            this.txt_convertedAmt.ReadOnly = true;
+            this.txt_convertedAmt.Size = new System.Drawing.Size(198, 20);
+            this.txt_convertedAmt.TabIndex = 1;
+            this.txt_convertedAmt.TextChanged += new System.EventHandler(this.txt_amount_TextChanged);
+            // 
+            // txt_convetedAmt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rdb_JapaneseYen);
+            this.Controls.Add(this.rdb_USdollars);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_convert);
+            this.Controls.Add(this.txt_convertedAmt);
+            this.Controls.Add(this.txt_amount);
+            this.Controls.Add(this.lbl_value);
+            this.Controls.Add(this.lbl_selectcurrency);
+            this.Controls.Add(this.lbl_amount);
+            this.Name = "txt_convetedAmt";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_amount;
+        private System.Windows.Forms.Label lbl_selectcurrency;
+        private System.Windows.Forms.Label lbl_value;
+        private System.Windows.Forms.TextBox txt_amount;
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.RadioButton rdb_USdollars;
+        private System.Windows.Forms.RadioButton rdb_JapaneseYen;
+        private System.Windows.Forms.TextBox txt_convertedAmt;
+    }
+}
+
---
+++ 17_PA9_AaronChan/17_PA9_AaronChan/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17_PA9_AaronChan
+{
+    public partial class txt_convetedAmt : Form
+    {
+        public txt_convetedAmt()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_amount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_convert_Click(object sender, EventArgs e)
+        {
+            double AmountEntered;
+            double convertedValue;
+
+            if((rdb_USdollars.Checked == false) || (rdb_JapaneseYen.Checked == false))
+            {
+                txt_convertedAmt.Text = "Select at least one type of currency to convert";
+            }
+
+            if(rdb_USdollars.Checked == true)
+            {
+                AmountEntered = double.Parse(txt_amount.Text);
+                convertedValue = AmountEntered * 0.74;
+
+                txt_convertedAmt.Text = convertedValue.ToString();
+            }
+
+            if (rdb_JapaneseYen.Checked == true)
+            {
+                AmountEntered = double.Parse(txt_amount.Text);
+                convertedValue = AmountEntered * 81.97;
+
+                txt_convertedAmt.Text = convertedValue.ToString();
+            }
+
+            try
+            {
+                txt_convertedAmt.Text = "Please enter numbers only";
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_convertedAmt.Text = "";
+            txt_amount.Text = "";
+
+            rdb_USdollars.Checked = false;
+            rdb_JapaneseYen.Checked = false;       
+        }
+    }
+}
